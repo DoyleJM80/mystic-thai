40972cb1127b8e13857b71eee6e66910f81abc13
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-07-26 09:08:43 -0400

    added header and footer

---
+++ src/components/BaseLayout.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Header from './Header.js';
+import Footer from './Footer.js';
+
+export default class BaseLayout extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    )
+  }
+}
