e1faece031a185ed07cfd74ddfbfd9506cab8cc1
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-08-01 20:46:50 -0400

    added styling to contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,18 @@ export default class Contact extends Component {
   render() {
     return(
       <div className="contact-container">
-        <h1>Contact me</h1>
+        <h1>Hours</h1><hr/>
+        <h3>MON: 9am ~ 9pm</h3>
+        <h3>TUE: 9am ~ 9pm</h3>
+        <h3>WED: 9am ~ 9pm</h3>
+        <h3>THU: 9am ~ 9pm</h3>
+        <h3>FRI: 9am ~ 11pm</h3>
+        <h3>SAT: 9am ~ 11pm</h3>
+        <h3>SUN: CLOSED</h3><hr/>
+        <h1>Phone Number</h1>
+        <h3>864-867-5309 ~ ask for Jenny</h3><hr/>
+        <h1>E-Mail</h1>
+        <h3>Thaifooooood@thaifood.com</h3>
       </div>
     );
   };
